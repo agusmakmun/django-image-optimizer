21d1ac27145f7ec720d57b50f87b2be1c03f390f
mcueto <cueto@live.cl>
2019-06-24 15:35:23 -0400

    Create Collaborator model to illustrate image resizing

--- image_optimizer_demo/app_demo/models.py
+++ image_optimizer_demo/app_demo/models.py
@@ -31,3 +31,36 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-created']
+
+
+class Collaborator(models.Model):
+    """
+    Collaborator model.
+
+    This model represents a Blog Collaborator(a.k.a. Writter) with a few
+    fields including a `profile_image` field which is an OptimizedImageField
+    instance with `optimized_image_output_size` and
+    `optimized_image_resize_method` arguments set.
+
+    This means that our Collaborator profile_image would be a resized
+    version of the source image, meant to keep a given screen resolution,
+    in this case (400, 300) pixels.
+    """
+
+    name = models.CharField(
+        max_length=100
+    )
+    profile_image = OptimizedImageField(
+        upload_to='uploads/collaborators/%Y/%m/%d',
+        optimized_image_output_size=(400, 300),
+        optimized_image_resize_method='cover'  # 'thumbnail' or 'cover'
+    )
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-created']
